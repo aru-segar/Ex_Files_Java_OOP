69213718ced91404f8fd0e7763259bf9a39357ef
aru_segar <arunigagnanasegaran@gmail.com>
2025-06-10 16:16:00 +0530

    Exploring a built-in Java class

--- Exercise Files/01_05/begin/Objects/src/Main.java
+++ Exercise Files/01_05/begin/Objects/src/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Color myTrunkColor = Tree.TRUNK_COLOR;
-    }
+        Color myDefaultWhite = Color.WHITE;
+        Color myDefaultBlue = Color.BLUE;
+
+        Color brighterBlue = myDefaultBlue.brighter();
+        }
 }
