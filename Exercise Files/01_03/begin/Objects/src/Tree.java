09fd14790f942c26ea809528d0e6b39bafcf44a1
aru_segar <arunigagnanasegaran@gmail.com>
2025-06-10 15:40:09 +0530

    Using concrete instances for application logic

--- Exercise Files/01_03/begin/Objects/src/Tree.java
+++ Exercise Files/01_03/begin/Objects/src/Tree.java
@@ -14,4 +14,12 @@ public class Tree {
         this.heightFt = this.heightFt + 10;
         this.trunkDiameterInches = this.trunkDiameterInches + 1;
     }
+
+    void announceTallTree() {
+        if (this.heightFt > 100) {
+            System.out.println("This is a very tall " + this.treeType + " tree!");
+        } else {
+            System.out.println("This tree is not that tall.");
+        }
+    }
 }
